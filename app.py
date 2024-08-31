45e38a2785dc662d2c39fab4eac79e89a942817a
Hameed Ibrahim <92448567+hameed2010@users.noreply.github.com>
2024-08-31 22:22:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,13 @@ import threading
 import requests 
 
 from googletrans import Translator
-
+def app(environ, start_response):
+        data = b"Hello, World!\n"
+        start_response("200 OK", [
+            ("Content-Type", "text/plain"),
+            ("Content-Length", str(len(data)))
+        ])
+        return iter([data])
 translator = Translator()
 
 CHANNEL_USERNAME = '@zxgn1'
